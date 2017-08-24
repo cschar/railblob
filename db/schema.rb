8aa0e0f921e7b2158381a180574b1ad71d84a9f9
cschar <codyscharfe@gmail.com>
2017-08-24 16:07:16 -0400

    singup page

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170824161806) do
+ActiveRecord::Schema.define(version: 20170824200226) do
 
   create_table "microposts", force: :cascade do |t|
     t.text "content"
