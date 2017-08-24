e4b1d99e82b223ecf3e93f800effb6aa9f98bed3
cschar <codyscharfe@gmail.com>
2017-08-24 17:58:22 -0400

    signup form/new/create/flash

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,38 @@
+
+require 'pry'
+
 class UsersController < ApplicationController
-  def new
+
+    def new
+      #@user sent to form
+      @user = User.new
+    end
+
+    def create
+      @user = User.new(user_params())
+      if @user.save
+        #rails flash variable hangs around to pass text into view
+        flash[:success] = "Welcome to the Sample App!"
+        redirect_to user_url(@user)
+        # redirect_to @user
+      else
+        render 'new'
+      end
+    end
+
+  def show
+
+    @user = User.find(params[:id])
+    # binding.pry
+    # debugger
   end
+
+
+  private
+    def user_params
+      params.require(:user).permit(:name, :email, :password,
+                                   :password_confirmation)
+    end
+
+
 end
