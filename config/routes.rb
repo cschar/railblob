fc1d08c3f623d1397a4a45f524a525ba6e531c5d
cschar <codyscharfe@gmail.com>
2017-08-24 12:50:16 -0400

    add some models

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,13 @@
 Rails.application.routes.draw do
+  resources :microposts
+  resources :users
   get 'welcome/index'
+  root 'application#hello'
+
 
   get 'welcome/show'
+
+  # root 'welcome/index'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
