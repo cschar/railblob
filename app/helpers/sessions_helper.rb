88387934769c01dd18bf8191d3cdb6d60ddaf25b
cschar <codyscharfe@gmail.com>
2017-08-25 13:08:18 -0400

    advanced login 9: permanent session w/ cookies

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -4,22 +4,57 @@ module SessionsHelper
     session[:user_id] = user.id
   end
 
-  # Returns the current logged-in user (if any).
-  def current_user
-    @current_user ||= User.find_by(id: session[:user_id])
-  end
-
   # Returns true if the user is logged in, false otherwise.
   def logged_in?
-    # puts @current_user
-
     !current_user.nil?
+  end
+
+
+  def remember(user)
+    user.remember
+
+    #securely encrypt cookie before placing in browser
+    cookies.permanent.signed[:user_id] = user.id
+
+    # This pattern of setting a cookie that expires 20 years in the future
+    # is so common that Rails has a special permanent method for it
+    #cookies[:remember_token] = { value:   remember_token,
+    #                             expires: 20.years.from_now.utc }
+    cookies.permanent[:remember_token] = user.remember_token
+    #remember_token is already a SecureRandom base64, no need for encryption
+    #w permanent.signed
+
+  end
+
+  # Returns the user corresponding to the remember token cookie.
+  def current_user
+    if session[:user_id]
+      @current_user ||= User.find_by(id: session[:user_id])
+    elsif cookies.signed[:user_id]
+      user = User.find_by(id: cookies.signed[:user_id])
+      #MATCH against remember_digest in db with remember_token
+      if user && user.authenticated?(cookies[:remember_token])
+        log_in user
+        @current_user = user
+      end
+    end
+  end
+
+
+
+  # Forgets a persistent session.
+  def forget(user)
+    user.forget  # set remember_digest to nil in DB
+    cookies.delete(:user_id)
+    cookies.delete(:remember_token)
   end
 
   # Logs out the current user.
   def log_out
+    forget(current_user)
     session.delete(:user_id)
     @current_user = nil
   end
+
 
 end
