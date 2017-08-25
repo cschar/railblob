8ce994fc04e67571403dcb69bd63fe6f56c4807c
cschar <codyscharfe@gmail.com>
2017-08-25 15:39:33 -0400

    delete user / allow user to be admin

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,8 +2,10 @@
 # require 'pry'
 
 class UsersController < ApplicationController
-  before_action :logged_in_user, only: [:edit, :update, :index]
+  before_action :logged_in_user, only: [:edit, :update, :index, :destroy]
   before_action :correct_user,   only: [:edit, :update]
+  before_action :admin_user,     only: :destroy
+
 
 
   def index
@@ -56,6 +58,13 @@ class UsersController < ApplicationController
     end
   end
 
+  def destroy
+    User.find(params[:id]).destroy
+    flash[:success] = "User deleted"
+    redirect_to users_url
+  end
+
+
 
   # Before filters   ###########################
 
@@ -74,6 +83,13 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
     redirect_to(root_url) unless current_user?(@user)
   end
+
+
+  # Confirms an admin user.
+  def admin_user
+    redirect_to(root_url) unless current_user.admin?
+  end
+
 
 
   private
--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -46,4 +46,33 @@ class UsersControllerTest < ActionDispatch::IntegrationTest
   end
 
 
+  test "should not allow the admin attribute to be edited via the web" do
+    log_in_as(@other_user)
+    assert_not @other_user.admin?
+    patch user_path(@other_user), params: {
+        user: { name:              'hey',
+                admin: true } }
+    # debugger
+    @other_user.reload
+
+    assert (@other_user.admin != true)
+    assert (@other_user.name == 'hey')
+  end
+
+  test "should redirect destroy when not logged in" do
+    assert_no_difference 'User.count' do
+      delete user_path(@user)
+    end
+    assert_redirected_to login_url
+  end
+
+  test "should redirect destroy when logged in as a non-admin" do
+    log_in_as(@other_user)
+    assert_no_difference 'User.count' do
+      delete user_path(@user)
+    end
+    assert_redirected_to root_url
+  end
+
+
 end
